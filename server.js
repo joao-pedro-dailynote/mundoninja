fade39f287e9fd773df85dbea761f39863f26999
kwny <126990056+guilhermekwny@users.noreply.github.com>
2023-10-26 17:50:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.get('/', ()=>{
+    
+})
+
 app.listen(port, () => {
   console.log(`Servidor está funcionando na porta ${port}`);
 });
